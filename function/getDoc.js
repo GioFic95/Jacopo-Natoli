6db9edd41f46d8932144d1a2dfcecbb47fd711e2
Giovanni Ficarra <giovannificarra95@gmail.com>
2019-07-20 02:07:17 +0200

    bug fix

--- function/getDoc.js
+++ function/getDoc.js
@@ -1,15 +1,38 @@
 const KEY = process.env.GOOGLE_DRIVE_KEY;
+const HOSTNAME = "docs.googleapis.com";
+const PATH = "/v1/documents/1aD2rEARRqD7GOv9yycQZhnYkE2NAKmovBHcUUoKDkZg?suggestionsViewMode=PREVIEW_WITHOUT_SUGGESTIONS&key="+KEY;
 const URL = "https://docs.googleapis.com/v1/documents/1aD2rEARRqD7GOv9yycQZhnYkE2NAKmovBHcUUoKDkZg?suggestionsViewMode=PREVIEW_WITHOUT_SUGGESTIONS&key="+KEY;
 
 exports.handler = async (event, context) => {
     let response, data;
 
-    let httpReq = new XMLHttpRequest();
-    httpReq.open("GET", URL, false);
-    httpReq.send();
-    if (httpReq.status === 200) {
-        response = httpReq.responseText;
-    }
+    const options = {
+        hostname: HOSTNAME,
+        path: PATH,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        }
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+            response = chunk;
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    req.end();
 
     data = await response.json();
 
