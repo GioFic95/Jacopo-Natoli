915f4b1c3cee410f842ecf81cd846ea35521183b
Giovanni Ficarra <giovannificarra95@gmail.com>
2019-07-20 02:21:16 +0200

    bug fix

--- function/getDoc.js
+++ function/getDoc.js
@@ -1,40 +1,23 @@
+const fetch = require('node-fetch');
 const KEY = process.env.GOOGLE_DRIVE_KEY;
-const HOSTNAME = "docs.googleapis.com";
-const PATH = "/v1/documents/1aD2rEARRqD7GOv9yycQZhnYkE2NAKmovBHcUUoKDkZg?suggestionsViewMode=PREVIEW_WITHOUT_SUGGESTIONS&key="+KEY;
 const URL = "https://docs.googleapis.com/v1/documents/1aD2rEARRqD7GOv9yycQZhnYkE2NAKmovBHcUUoKDkZg?suggestionsViewMode=PREVIEW_WITHOUT_SUGGESTIONS&key="+KEY;
 
 exports.handler = async (event, context) => {
     let response, data;
 
-    const options = {
-        hostname: HOSTNAME,
-        path: PATH,
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        }
-    };
-
-    const req = http.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-            response = chunk;
+    try {
+        response = await fetch(URL, {
+            method: "GET",
+            headers: new Headers({
+                "Accept": "application/json"
+            })
         });
-        res.on('end', () => {
-            console.log('No more data in response.');
-        });
-    });
 
-    req.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-    });
-
-    req.end();
-
-    data = await response.json();
+        data = await response.json();
+    } catch (error) {
+        console.error(error.message);
+        return;
+    }
 
     return {
         statusCode: 200,
