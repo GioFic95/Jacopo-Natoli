f74d33d54f021cb242de3eb68c9c855cadd5e8bd
Giovanni Ficarra <giovannificarra95@gmail.com>
2019-07-20 05:07:51 +0200

    bug fix :(

--- function/getDoc.js
+++ function/getDoc.js
@@ -4,7 +4,7 @@ const TOKEN = process.env.ACCESS_TOKEN;
 const URL = "https://docs.googleapis.com/v1/documents/1aD2rEARRqD7GOv9yycQZhnYkE2NAKmovBHcUUoKDkZg?suggestionsViewMode=PREVIEW_WITHOUT_SUGGESTIONS&key=";
 
 exports.handler = async (event, context) => {
-    let response, data, out;
+    let response, data, out, id;
 
     try {
         response = await fetch(URL, {
@@ -19,7 +19,7 @@ exports.handler = async (event, context) => {
         console.log("data:", data);
         data = data.body.content;
 
-        let id = Math.floor(Math.random() * data.length-1);
+        id = Math.floor(Math.random() * data.length-1);
         console.log("id:", id);
 
         out = [];
